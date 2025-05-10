b358f9d9d3d3ddb446c58e7b5cbb0cdddd7bb139
Mustafa <mostafamohamed12434@gmail.com>
2025-05-11 00:21:47 +0300

    .

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,6 @@
 const User = require("../models/userschema");
+
 const getUsers = async (req, res) => {
-  /*  res.status(200).json({
-      status: 200,
-      data: { data: users, message: "Users fetched successfully" },
-    });*/
   try {
     const users = await User.find({});
     res.status(200).json({
@@ -13,7 +10,6 @@ const getUsers = async (req, res) => {
   } catch (err) {
     res.status(500).json({ status: 500, message: "Server error" });
   }
-
 };
 
 const addUsers = async (req, res) => {
@@ -25,24 +21,22 @@ const deleteUsers = async (req, res) => {
   console.log("delete users");
   res.status(200).json({ message: "User deleted (dummy response)" });
 };
+
 const putUsers = async (req, res) => {
   console.log("put Users");
-  res.status(200).json({ message: "User deleted (dummy response)" });
+  res.status(200).json({ message: "User updated (dummy response)" });
 };
 
 const getSingleUsers = async (req, res) => {
   const { userId } = req.params;
-
   try {
     const singleUser = await User.findById(userId);
-
     if (!singleUser) {
       return res.status(404).json({
         status: 404,
         data: { data: null, message: "Invalid user ID" },
       });
     }
-
     return res.status(200).json({
       status: 200,
       data: { data: singleUser, message: "User fetched successfully" },
--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,16 @@ const {
   getSingleUsers,
 } = require("../controllers/users");
 const { signup, login } = require("../controllers/auth");
+
 user_routes.route("/").get(getUsers);
 user_routes.route("/Signup").post(signup);
 user_routes.route("/login").get(login);
+
 user_routes
   .route("/:userId")
   .get(getSingleUsers)
   .post(addUsers)
   .put(putUsers)
   .delete(deleteUsers);
+
 module.exports = { user_routes };
