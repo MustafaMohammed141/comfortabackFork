28e7cb422290cf6d7c8999ccd3f6eb92861a31ec
Mustafa <68327651+MustafaMohammed141@users.noreply.github.com>
2025-05-10 23:45:50 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,54 +1,15 @@
+const User = require("../models/userschema");
 
-
-const  User =require("../models/userschema");
-  const getUsers = async (req, res) => {
-
-  /*  res.status(200).json({
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({
       status: 200,
       data: { data: users, message: "Users fetched successfully" },
-    });*/
-    try {
-      const users = await User.find({});
-      res.status(200).json({
-        status: 200,
-        data: { data: users, message: "Users fetched successfully" },
-      });
-    } catch (err) {
-      res.status(500).json({ status: 500, message: "Server error" });
-    }
-  };
-  
-  const addUsers = async (req, res) => {
-    console.log("add users");
-    res.status(201).json({ message: "User added (dummy response)" });
-  };
-  
-  const deleteUsers = async (req, res) => {
-    console.log("delete users");
-    res.status(200).json({ message: "User deleted (dummy response)" });
-  };
-  
-const getSingleUsers = async (req, res) => {
-  const { userId } = req.params;
-
-  try {
-    const singleUser = await User.findById(userId);
-
-    if (!singleUser) {
-      return res.status(404).json({
-        status: 404,
-        data: { data: null, message: "Invalid user ID" },
-      });
-    }
-
-    return res.status(200).json({
-      status: 200,
-      data: { data: singleUser, message: "User fetched successfully" },
     });
   } catch (err) {
     res.status(500).json({ status: 500, message: "Server error" });
   }
-
 };
 
 const addUsers = async (req, res) => {
@@ -60,24 +21,22 @@ const deleteUsers = async (req, res) => {
   console.log("delete users");
   res.status(200).json({ message: "User deleted (dummy response)" });
 };
+
 const putUsers = async (req, res) => {
   console.log("put Users");
-  res.status(200).json({ message: "User deleted (dummy response)" });
+  res.status(200).json({ message: "User updated (dummy response)" });
 };
 
 const getSingleUsers = async (req, res) => {
   const { userId } = req.params;
-
   try {
     const singleUser = await User.findById(userId);
-
     if (!singleUser) {
       return res.status(404).json({
         status: 404,
         data: { data: null, message: "Invalid user ID" },
       });
     }
-
     return res.status(200).json({
       status: 200,
       data: { data: singleUser, message: "User fetched successfully" },
