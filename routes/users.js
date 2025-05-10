c4852565ff9b48bf42db350272f284dbb44d8391
Mustafa <68327651+MustafaMohammed141@users.noreply.github.com>
2025-05-10 16:56:01 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -15,9 +15,9 @@ user_routes.route("/Signup").post(signup);
 user_routes.route("/login").get(login);
 user_routes
   .route("/:userId")
-  .get(checkAuth, getSingleUsers)
-  .post(checkAuth, postUsers)
-  .put(checkAuth, putUsers)
-  .delete(checkAuth, deleteUsers);
+  .get(getSingleUsers)
+  .post(postUsers)
+  .put(putUsers)
+  .delete(deleteUsers);
 
 module.exports = { user_routes };
